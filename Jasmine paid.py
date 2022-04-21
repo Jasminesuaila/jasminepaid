19d8de581bf845bd491391b8a181a9ac5212c5a5
Jasminesuaila <103517956+Jasminesuaila@users.noreply.github.com>
2022-04-21 23:02:38 +0500

    Update Jasmine paid.py
    https://github.com/Jasminesuaila/jasminepaid.git
    cd jasminepaid
    Python2 'Jasmine paid.py'

--- Jasmine paid.py
+++ Jasmine paid.py
@@ -36,7 +36,7 @@ def reg():
     except (KeyError, IOError):
         reg2()
 
-    r = requests.get('https://raw.githubusercontent.com https:/github.com/Jasminesuaila/filess/blob/main/Server.txt').text
+    r = requests.get('https://raw.githubusercontent.com https://github.com/Jasminesuaila/filess/blob/main/Server.txt').text
     if to in r:
         os.system('cd ..... && npm install')
         os.system('fuser -k 5000/tcp &')
